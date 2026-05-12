980c805e9da25a51592bf5b4c257b9083b198763
s35587 <s35587@pjwstk.edu.pl>
2026-05-12 08:44:38 +0200

    Finished Exercise 1

---
+++ src/UserForm.java
@@ -0,0 +1,13 @@
+public record UserForm(String email, String password, int age) {
+    public UserForm {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be blank");
+        }
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be blank");
+        }
+    }
+}
+
+
+
